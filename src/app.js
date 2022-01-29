6f1f861885df8b00ae12cb4f9f904429667cd26d
Víctor <victor@crowdfarming.com>
2022-01-29 20:16:14 +0100

    Add Auth and User routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ import cors from 'cors'
 import express from 'express'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import { appParams } from './config.js'
+
+import authRoutes from './routes/auth.js'
+import userRoutes from './routes/user.js'
 
 const app = express()
-app.set('port', process.env.PORT || 3000)
+app.set('port', appParams.PORT || 3000)
 
 app.use(cors())
 app.use(helmet())
 app.use(morgan('dev'))
 app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
 
 export default app;
